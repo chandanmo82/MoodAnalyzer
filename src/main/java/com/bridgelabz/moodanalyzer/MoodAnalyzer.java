6dc23c1297f01c9d38e1003a7f31c019c7169474
Chandan <chandanmohanty11111>
2021-10-21 18:08:36 +0530

    Initial Commit

---
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.moodanalyzer;
+
+public class MoodAnalyzer {
+
+    String message;
+
+    public MoodAnalyzer() {
+
+    }
+
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    public String analyzeMood() {
+
+        if (this.message.contains("Sad")) {
+            return "SAD";
+        } else {
+            return "HAPPY";
+        }
+    }
+}
+
---
+++ src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.moodanalyzer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+
+    @Test
+    public void givenMessage_WhenSad_ShouldReturnSad() throws Exception {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a Sad message");
+        String mood = moodAnalyzer.analyzeMood();
+        Assert.assertEquals("SAD",mood);
+
+    }
+    @Test
+    public void givenMessage_WhenHappy_ShouldReturnHappy() throws Exception {
+
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a Happy message");
+        String mood = moodAnalyzer.analyzeMood();
+        Assert.assertEquals("HAPPY",mood);
+    }
+
+}
